3bd1a68a35df52915cf7cd1845c0897c9d2e633d
Diego Veiga Santos <sdiegoveiga@gmail.com>
2020-03-18 10:16:33 -0300

    chore: commit initial

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,12 @@
+export default {
+  title: 'dark',
+
+  colors: {
+    
+    primary: '#2B2D42',
+    secundary: '#8D99AE',
+
+    background: '#222',
+    text: '#fff'
+  },
+}
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,11 @@
+export default {
+  title: 'light',
+
+  colors: {
+    primary: '#E36588',
+    secundary: '#9A275A',
+
+    background: '#f5f5f5',
+    text: '#333'
+  },
+}
